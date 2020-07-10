47bd8491760e846c0d01cd2619d782b61508084c
edisonandrade <Edisonweconnect2019>
2020-07-10 16:41:48 -0500

    Actualizacion

--- qrgenerator/QRGenerator.py
+++ qrgenerator/QRGenerator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+import os
 
 import pyqrcode
-import os, subprocess, shutil
 from PIL import Image, ImageDraw, ImageFont
 
 
@@ -14,19 +14,6 @@ class QRGeneratorLogic:
         self.__file_location__ = file_location
         self.__qr_code__ = qr_code
         self.__crypt_text__ = crypt_text
-        # see is gs is avaible
-        if shutil.which("gs") is None:
-            print("GhostScrip is not avaible, search for it")
-            try:
-                gs = subprocess.Popen(["which", "gs"], stdout=subprocess.PIPE)
-                gs_path = gs.stdout.read()
-                gs_path = gs_path.decode() if isinstance(gs_path, bytes) else gs_path
-                print("GhostScrip found in", gs_path)
-                os.environ["PATH"] += ":" + os.path.dirname(gs_path)
-            except Exception as e:
-                raise Warning("GhostScrip not found, this program may fail")
-        del subprocess
-        del shutil
 
     def generate_qr_code(self):
         eps_file_location = str(self.__file_location__)+'.eps'
@@ -41,7 +28,7 @@ class QRGeneratorLogic:
         pic.save(png_file_location)
         pic = Image.open(png_file_location)
         draw = ImageDraw.Draw(pic)
-        font = ImageFont.truetype("/opt/wildfly/standalone/data/python/ModuloQR/qrgenerator/OpenSans-Bold.ttf", 60)
+        font = ImageFont.truetype(str(os.path.dirname(os.path.abspath(__file__))) + "/OpenSans-Bold.ttf", 60)
         lines = self.__qr_code__.splitlines()
         w = font.getsize(max(lines, key=lambda s: len(s)))[0]
         x, y = pic.size
