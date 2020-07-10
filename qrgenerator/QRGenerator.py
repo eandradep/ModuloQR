45833ba76bbef5ee343369b551851fcbb375c3b7
edisonandrade <Edisonweconnect2019>
2020-07-10 16:19:50 -0500

    Modificacion para Agregar COdigo en la Parte Baja y diferenciar entre creacion y edicion de informacion, se separa el metodo para crear el Codigo QR

--- qrgenerator/QRGenerator.py
+++ qrgenerator/QRGenerator.py
@@ -2,7 +2,6 @@
 
 import pyqrcode
 from PIL import Image, ImageDraw, ImageFont
-import os
 
 
 class QRGeneratorLogic:
@@ -21,24 +20,39 @@ class QRGeneratorLogic:
         print('url eps:'+eps_file_location)
         print('url eps:'+png_file_location)
         url = pyqrcode.create(self.__crypt_text__)
+        print('paso 1:' )
         url.eps(eps_file_location, scale=10)
+        print('paso 2:')
         target_bounds = (2080, 2080)
+        print('paso 3:')
         pic = Image.open(eps_file_location)
+        print('paso 4:')
         pic.load(scale=10)
+        print('paso 5:')
         if pic.mode in ('P', '1'):
             pic = pic.convert("RGB")
+        print('paso 6:')
         ratio = min(target_bounds[0] / pic.size[0],
                     target_bounds[1] / pic.size[1])
+        print('paso 7:')
         new_size = (int(pic.size[0] * ratio), int(pic.size[1] * ratio))
+        print('paso 8:')
         # Resize to fit the target size
         pic = pic.resize(new_size, Image.ANTIALIAS)
+        print('paso 9:')
         draw = ImageDraw.Draw(pic)
+        print('paso 10:')
         font = ImageFont.truetype("/opt/wildfly/standalone/data/python/ModuloQR/qrgenerator/OpenSans-Bold.ttf", 60)
+        print('paso 11:')
         lines = self.__qr_code__.splitlines()
+        print('paso 12:')
         w = font.getsize(max(lines, key=lambda s: len(s)))[0]
+        print('paso 13:')
         x, y = pic.size
         x /= 2
         x -= w / 2
         y = y - 100
+        print('paso 14:')
         draw.text((x, y), self.__qr_code__, font=font, fill="Black")
+        print('paso 15:')
         pic.save(png_file_location)
