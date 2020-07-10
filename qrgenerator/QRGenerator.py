0e0caac263b57ffb8904577c1385a63c0c31a172
edisonandrade <Edisonweconnect2019>
2020-07-10 16:53:00 -0500

    Actualizacion

--- qrgenerator/QRGenerator.py
+++ qrgenerator/QRGenerator.py
@@ -2,7 +2,6 @@
 import os
 
 import pyqrcode
-import os, subprocess, shutil
 from PIL import Image, ImageDraw, ImageFont
 
 
@@ -15,32 +14,17 @@ class QRGeneratorLogic:
         self.__file_location__ = file_location
         self.__qr_code__ = qr_code
         self.__crypt_text__ = crypt_text
-        if shutil.which("gs") is None:
-            print("GhostScrip is not avaible, search for it")
-            try:
-                gs = subprocess.Popen(["which", "gs"], stdout=subprocess.PIPE)
-                gs_path = gs.stdout.read()
-                gs_path = gs_path.decode() if isinstance(gs_path, bytes) else gs_path
-                print("GhostScrip found in", gs_path)
-                os.environ["PATH"] += ":" + os.path.dirname(gs_path)
-            except Exception as e:
-                raise Warning("GhostScrip not found, this program may fail")
-
-        del subprocess
-        del shutil
 
     def generate_qr_code(self):
-        eps_file_location = str(self.__file_location__)+'.eps'
-        png_file_location = str(self.__file_location__)+'.png'
+        eps_file_location = str(self.__file_location__)+'.png'
         url = pyqrcode.create(self.__crypt_text__)
-        url.eps(eps_file_location, scale=10)
+        url.png(eps_file_location, scale=10)
         # Guardar EPS
         pic = Image.open(eps_file_location)
         if pic.mode in ('P', '1'):
             pic = pic.convert("RGB")
         # Resize to fit the target size
-        pic.save(png_file_location)
-        pic = Image.open(png_file_location)
+        pic = Image.open(eps_file_location)
         draw = ImageDraw.Draw(pic)
         font = ImageFont.truetype(str(os.path.dirname(os.path.abspath(__file__))) + "/OpenSans-Bold.ttf", 60)
         lines = self.__qr_code__.splitlines()
@@ -50,4 +34,4 @@ class QRGeneratorLogic:
         x -= w / 2
         y = y - 100
         draw.text((x, y), self.__qr_code__, font=font, fill="Black")
-        pic.save(png_file_location)
+        pic.save(eps_file_location)
