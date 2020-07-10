84ba81376903a5c03557754d69b37ee1de083073
edisonandrade <Edisonweconnect2019>
2020-07-10 16:59:00 -0500

    Actualizacion

--- qrgenerator/QRGenerator.py
+++ qrgenerator/QRGenerator.py
@@ -16,18 +16,22 @@ class QRGeneratorLogic:
         self.__crypt_text__ = crypt_text
 
     def generate_qr_code(self):
-        eps_file_location = str(self.__file_location__)+'.png'
+
+        eps_file_location = str(self.__file_location__) + '.png'
         url = pyqrcode.create(self.__crypt_text__)
         url.png(eps_file_location, scale=10)
-        # Guardar EPS
+        target_bounds = (2080, 2080)
         pic = Image.open(eps_file_location)
         pic.load(scale=10)
         if pic.mode in ('P', '1'):
             pic = pic.convert("RGB")
+        ratio = min(target_bounds[0] / pic.size[0],
+                    target_bounds[1] / pic.size[1])
+        new_size = (int(pic.size[0] * ratio), int(pic.size[1] * ratio))
         # Resize to fit the target size
-        pic = Image.open(eps_file_location)
+        pic = pic.resize(new_size, Image.ANTIALIAS)
         draw = ImageDraw.Draw(pic)
-        font = ImageFont.truetype(str(os.path.dirname(os.path.abspath(__file__))) + "/OpenSans-Bold.ttf", 60)
+        font = ImageFont.truetype(str(os.path.dirname(os.path.abspath(__file__)))+"/OpenSans-Bold.ttf", 60)
         lines = self.__qr_code__.splitlines()
         w = font.getsize(max(lines, key=lambda s: len(s)))[0]
         x, y = pic.size
@@ -36,3 +40,4 @@ class QRGeneratorLogic:
         y = y - 100
         draw.text((x, y), self.__qr_code__, font=font, fill="Black")
         pic.save(eps_file_location)
+        
