87943c471ec09bc11f91074fcd9c62d2017273b6
edisonandrade <Edisonweconnect2019>
2020-07-10 16:35:42 -0500

    Actualizacion

--- qrgenerator/QRGenerator.py
+++ qrgenerator/QRGenerator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import pyqrcode
-import subprocess
+import os, subprocess, shutil
 from PIL import Image, ImageDraw, ImageFont
 
 
@@ -14,7 +14,19 @@ class QRGeneratorLogic:
         self.__file_location__ = file_location
         self.__qr_code__ = qr_code
         self.__crypt_text__ = crypt_text
-        subprocess.check_call(['gs', '--version'])
+        # see is gs is avaible
+        if shutil.which("gs") is None:
+            print("GhostScrip is not avaible, search for it")
+            try:
+                gs = subprocess.Popen(["which", "gs"], stdout=subprocess.PIPE)
+                gs_path = gs.stdout.read()
+                gs_path = gs_path.decode() if isinstance(gs_path, bytes) else gs_path
+                print("GhostScrip found in", gs_path)
+                os.environ["PATH"] += ":" + os.path.dirname(gs_path)
+            except Exception as e:
+                raise Warning("GhostScrip not found, this program may fail")
+        del subprocess
+        del shutil
 
     def generate_qr_code(self):
         eps_file_location = str(self.__file_location__)+'.eps'
